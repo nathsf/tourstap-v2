bc17162f24234ade3884e1e33fbd8e29172cf972
Nathaly <nathaly@laboratoria.la>
2016-09-02 16:01:16 -0500

    menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,8 @@
-//countdown
-function clock(numeroID) {
- $("#clockdiv"+numeroID)
-   .countdown("2016/07/24", function(event) {
-     $(this).text(
-       event.strftime('%D d %H:%M:%S')
-     );
-   });
-  $("#clockmovil"+numeroID)
-     .countdown("2016/07/24", function(event) {
-       $(this).text(
-         event.strftime('%D d %H:%M:%S')
-       );
-     });
-}
-
-//upload selfie 
-
-function selfie(numeroInput) {
-    $("#fileToUpload"+numeroInput).change(function(){
-        $("#file"+numeroInput).val($("#fileToUpload"+numeroInput).val());
-
-        var val = $(this).val();
-
-        switch(val.substring(val.lastIndexOf('.') + 1).toLowerCase()){
-            case 'gif': case 'jpg': case 'png': case 'jpeg':
-                break;
-            default:
-                $(this).val('');
-                // error message here
-                alert("Formato invalido. Suba una foto en formato jpg, jpeg, png o gif");
-                break;
-        }
-
-    });
-    
-
-    $("#fileToUpload"+numeroInput).click(function(){
-        $(".input-imagen").show();
-    });
-    $("#submit"+numeroInput).click(function(){
-        $(".input-imagen").hide();
-        // $(this).prop( "disabled", true );
-    });
-}
-
-
-
+/*carousel*/
+$('.slide-descuentos').owlCarousel({
+            responsiveClass: true,
+            autoplay:false,
+            items: 3,
+            dots: false,
+            nav: false,
+        });
