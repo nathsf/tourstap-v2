7384756b71282343d10d37877393e25432311a2d
Nathaly <nathaly@laboratoria.la>
2016-09-05 17:35:20 -0500

    editar perfil

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,24 @@ $('.slide-descuentos').owlCarousel({
             dots: false,
             nav: false,
         });
+
+//upload selfie 
+
+function foto(numeroInput) {
+    $("#fileToUpload"+numeroInput).change(function(){
+        $("#file"+numeroInput).val($("#fileToUpload"+numeroInput).val());
+
+        var val = $(this).val();
+
+        switch(val.substring(val.lastIndexOf('.') + 1).toLowerCase()){
+            case 'gif': case 'jpg': case 'png': case 'jpeg':
+                break;
+            default:
+                $(this).val('');
+                // error message here
+                alert("Formato invalido. Suba una foto en formato jpg, jpeg, png o gif");
+                break;
+        }
+
+    });
+}
